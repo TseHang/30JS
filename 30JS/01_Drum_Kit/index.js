f6ec3e388a5984319ac4d3f72c47a83d94805b9c
TseHang <a54383813@gmail.com>
2017-12-28 14:12:07 +0800

    Finish day01

--- 30JS/01_Drum_Kit/index.js
+++ 30JS/01_Drum_Kit/index.js
@@ -1,3 +1,21 @@
-window.addEventListener('keydown', event => {
-  console.log(event.keyCode);
-});
+function playSound(e) {
+  const keyCode = e.keyCode || this.getAttribute('data-key');
+  const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+  const button = document.querySelector(`.key-button[data-key="${keyCode}"]`);
+  if (!audio) return;
+
+  button.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+}
+
+function removeTransition(e) {
+  e.target.classList.remove('playing');
+}
+
+const keyButtons = Array.from(document.querySelectorAll('.key-button'));
+
+keyButtons.forEach(keyButton => keyButton.addEventListener('transitionend', removeTransition));
+keyButtons.forEach(keyButton => keyButton.addEventListener('click', playSound));
+window.addEventListener('keydown', playSound);
+
--- dist/js/01_Drum_Kit/index.js
+++ dist/js/01_Drum_Kit/index.js
@@ -1,5 +1,26 @@
 'use strict';
 
-window.addEventListener('keydown', function (event) {
-  console.log(event.keyCode);
+function playSound(e) {
+  var keyCode = e.keyCode || this.getAttribute('data-key');
+  var audio = document.querySelector('audio[data-key="' + keyCode + '"]');
+  var button = document.querySelector('.key-button[data-key="' + keyCode + '"]');
+  if (!audio) return;
+
+  button.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+}
+
+function removeTransition(e) {
+  e.target.classList.remove('playing');
+}
+
+var keyButtons = Array.from(document.querySelectorAll('.key-button'));
+
+keyButtons.forEach(function (keyButton) {
+  return keyButton.addEventListener('transitionend', removeTransition);
 });
+keyButtons.forEach(function (keyButton) {
+  return keyButton.addEventListener('click', playSound);
+});
+window.addEventListener('keydown', playSound);
