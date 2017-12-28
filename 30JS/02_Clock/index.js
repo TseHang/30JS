22efa7e3b739d011b7a916372577027de2ccdcb9
TseHang <a54383813@gmail.com>
2017-12-28 18:42:26 +0800

    Update day 02_Clock

---
+++ 30JS/02_Clock/index.js
@@ -0,0 +1,4 @@
+const now = new Date();
+console.log(now.getSeconds());
+console.log(now.getHours());
+console.log(now.getMinutes());
---
+++ dist/js/02_Clock/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var now = new Date();
+console.log(now.getSeconds());
+console.log(now.getHours());
+console.log(now.getMinutes());
