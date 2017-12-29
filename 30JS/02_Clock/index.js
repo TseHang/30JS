4abb22bd7a557f1eb82002e6b3e5de0da12268df
TseHang <a54383813@gmail.com>
2017-12-29 18:57:00 +0800

    Finish day02

--- 30JS/02_Clock/index.js
+++ 30JS/02_Clock/index.js
@@ -1,4 +1,34 @@
-const now = new Date();
-console.log(now.getSeconds());
-console.log(now.getHours());
-console.log(now.getMinutes());
+const handSecond = document.querySelector('.second-hand');
+const handMin = document.querySelector('.min-hand');
+const handHour = document.querySelector('.hour-hand');
+
+const update = () => {
+  const now = new Date();
+  const secondDegree = transformSeconds(now.getSeconds());
+  const minDegree = transformMins(now.getMinutes());
+  const hourDegree = transformHours(now.getHours(), now.getMinutes());
+
+  handSecond.style.transform = `rotate(${-90 + secondDegree}deg)`;
+  handMin.style.transform = `rotate(${-90 + minDegree}deg)`;
+  handHour.style.transform = `rotate(${-90 + hourDegree}deg)`;
+  console.log(123);
+
+  function transformSeconds(seconds) {
+    return (360 / 60) * seconds;
+  }
+
+  function transformMins(mins) {
+    return (360 / 60) * mins;
+  }
+
+  function transformHours(hours, mins) {
+    return (360 / 12) * hours + (30 / 60) * mins;
+  }
+};
+
+
+window.setInterval(update, 1000);
+
+// console.log(now.getSeconds());
+// console.log(now.getHours());
+// console.log(now.getMinutes());
--- dist/js/02_Clock/index.js
+++ dist/js/02_Clock/index.js
@@ -1,6 +1,35 @@
-"use strict";
+'use strict';
 
-var now = new Date();
-console.log(now.getSeconds());
-console.log(now.getHours());
-console.log(now.getMinutes());
+var handSecond = document.querySelector('.second-hand');
+var handMin = document.querySelector('.min-hand');
+var handHour = document.querySelector('.hour-hand');
+
+var update = function update() {
+  var now = new Date();
+  var secondDegree = transformSeconds(now.getSeconds());
+  var minDegree = transformMins(now.getMinutes());
+  var hourDegree = transformHours(now.getHours(), now.getMinutes());
+
+  handSecond.style.transform = 'rotate(' + (-90 + secondDegree) + 'deg)';
+  handMin.style.transform = 'rotate(' + (-90 + minDegree) + 'deg)';
+  handHour.style.transform = 'rotate(' + (-90 + hourDegree) + 'deg)';
+  console.log(123);
+
+  function transformSeconds(seconds) {
+    return 360 / 60 * seconds;
+  }
+
+  function transformMins(mins) {
+    return 360 / 60 * mins;
+  }
+
+  function transformHours(hours, mins) {
+    return 360 / 12 * hours + 30 / 60 * mins;
+  }
+};
+
+window.setInterval(update, 1000);
+
+// console.log(now.getSeconds());
+// console.log(now.getHours());
+// console.log(now.getMinutes());
