8f91294103f00ba90c96292606fe9d016de59a0c
TseHang <a54383813@gmail.com>
2018-01-09 23:42:27 +0800

    Add day13 sidein

--- 30JS/13_Sidein/index.js
+++ 30JS/13_Sidein/index.js
@@ -1,25 +1,31 @@
 const sliderImages = document.querySelectorAll('.slide-in');
 let loadAll = false;
-// window.addEventListener('scroll', debounce(checkSlide));
-window.addEventListener('scroll', checkSlide);
+
+window.addEventListener('scroll', debounce(checkSlide));
 
 // 如果單純使用scroll來操作的話，每次的畫面滾動都會有大量事件被觸發，
 // 會對效能上造成影響，所以多寫了一個 debounce 來使觸發間隔為20毫秒以上：
-// function debounce(func, wait = 20, immediate = true) {
-//   let timeout;
-//   return function (...argument) {
-//     const context = this;
-//     const args = argument;
-//     console.log(this, a)
-//   }
-// }
+function debounce(func, wait = 20, immediate = true) {
+  let timeout;
+  return function (...args) {
+    const context = this;
+    const callNow = immediate && !timeout;
+    const later = () => {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
 
 function checkSlide() {
+  console.log('checkSlide!');
   sliderImages.forEach((sliderImage, i, array) => {
     if (loadAll) return;
-    console.log(123);
     const slideInAt = window.scrollY + window.innerHeight;
-    const isShown = slideInAt > sliderImage.offsetTop;
+    const isShown = slideInAt > sliderImage.offsetTop - (sliderImage.height / 2);
     if (isShown) {
       const imageLink = sliderImage.dataset.imglink;
       sliderImage.setAttribute('src', imageLink);
--- dist/js/13_Sidein/index.js
+++ dist/js/13_Sidein/index.js
@@ -2,26 +2,39 @@
 
 var sliderImages = document.querySelectorAll('.slide-in');
 var loadAll = false;
-// window.addEventListener('scroll', debounce(checkSlide));
-window.addEventListener('scroll', checkSlide);
+
+window.addEventListener('scroll', debounce(checkSlide));
 
 // 如果單純使用scroll來操作的話，每次的畫面滾動都會有大量事件被觸發，
 // 會對效能上造成影響，所以多寫了一個 debounce 來使觸發間隔為20毫秒以上：
-// function debounce(func, wait = 20, immediate = true) {
-//   let timeout;
-//   return function (...argument) {
-//     const context = this;
-//     const args = argument;
-//     console.log(this, a)
-//   }
-// }
+function debounce(func) {
+  var wait = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 20;
+  var immediate = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : true;
+
+  var timeout = void 0;
+  return function () {
+    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    var context = this;
+    var callNow = immediate && !timeout;
+    var later = function later() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
 
 function checkSlide() {
+  console.log('checkSlide!');
   sliderImages.forEach(function (sliderImage, i, array) {
     if (loadAll) return;
-    console.log(123);
     var slideInAt = window.scrollY + window.innerHeight;
-    var isShown = slideInAt > sliderImage.offsetTop;
+    var isShown = slideInAt > sliderImage.offsetTop - sliderImage.height / 2;
     if (isShown) {
       var imageLink = sliderImage.dataset.imglink;
       sliderImage.setAttribute('src', imageLink);
