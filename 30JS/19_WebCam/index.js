41ca2b4aa2744fa2d204c97a615e923607143ab2
TseHang <a54383813@gmail.com>
2018-01-23 16:57:11 +0800

    Add dat19WebCam

--- 30JS/19_WebCam/index.js
+++ 30JS/19_WebCam/index.js
@@ -4,15 +4,50 @@ const video = document.querySelector('.player');
 const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 
+const redEffect = (pixels) => {
+  const renderPixels = pixels;
+  // 透過迴圈將取回的所有像素資料跑一次，i +=4 是因為四個一組(r,g,b,alpha）
+  for (let i = 0; i < renderPixels.data.length; i += 4) {
+    // 下面組合就是單純把R(紅色)增強達到紅色濾鏡的效果
+    renderPixels.data[i + 0] = renderPixels.data[i + 0] + 100;
+    renderPixels.data[i + 1] = renderPixels.data[i + 1] - 50;
+    renderPixels.data[i + 2] = renderPixels.data[i + 2] * 0.5;
+  }
+  return renderPixels;
+};
+
+const rgbColorSplit = (pixels) => {
+  const renderPixels = pixels;
+  for (let i = 0; i < renderPixels.data.length; i += 4) {
+    renderPixels.data[i - 150] = renderPixels.data[i + 0];
+    renderPixels.data[i + 500] = renderPixels.data[i + 1];
+    renderPixels.data[i - 550] = renderPixels.data[i + 2];
+  }
+  return renderPixels;
+};
+
 function getVideo() {
   navigator.mediaDevices.getUserMedia({
     video: true,
     audioL: false,
   }).then((localMediaStream) => {
-    console.log(localMediaStream);
     video.src = window.URL.createObjectURL(localMediaStream);
     video.play();
   }).catch(err => console.error('ERROR: ', err));
+}
+
+function paintToCanavas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+
+    const pixels = ctx.getImageData(0, 0, width, height);
+    ctx.putImageData(rgbColorSplit(pixels), 0, 0);
+  });
 }
 
 function takePhoto() {
@@ -23,13 +58,9 @@ function takePhoto() {
   const link = document.createElement('a');
   link.href = data;
   link.setAttribute('download', 'photo');
+  link.textContent = 'Download Image';
   link.innerHTML = `<img src="${data}" alt="photo"/>`;
   strip.insertBefore(link, strip.firstChild);
-  // strip 比較像是 霸凌課
-}
-
-function paintToCanavas() {
-  
 }
 
 getVideo();
--- dist/js/19_WebCam/index.js
+++ dist/js/19_WebCam/index.js
@@ -6,16 +6,51 @@ var video = document.querySelector('.player');
 var canvas = document.querySelector('.photo');
 var ctx = canvas.getContext('2d');
 
+var redEffect = function redEffect(pixels) {
+  var renderPixels = pixels;
+  // 透過迴圈將取回的所有像素資料跑一次，i +=4 是因為四個一組(r,g,b,alpha）
+  for (var i = 0; i < renderPixels.data.length; i += 4) {
+    // 下面組合就是單純把R(紅色)增強達到紅色濾鏡的效果
+    renderPixels.data[i + 0] = renderPixels.data[i + 0] + 100;
+    renderPixels.data[i + 1] = renderPixels.data[i + 1] - 50;
+    renderPixels.data[i + 2] = renderPixels.data[i + 2] * 0.5;
+  }
+  return renderPixels;
+};
+
+var rgbColorSplit = function rgbColorSplit(pixels) {
+  var renderPixels = pixels;
+  for (var i = 0; i < renderPixels.data.length; i += 4) {
+    renderPixels.data[i - 150] = renderPixels.data[i + 0];
+    renderPixels.data[i + 500] = renderPixels.data[i + 1];
+    renderPixels.data[i - 550] = renderPixels.data[i + 2];
+  }
+  return renderPixels;
+};
+
 function getVideo() {
   navigator.mediaDevices.getUserMedia({
     video: true,
     audioL: false
   }).then(function (localMediaStream) {
-    console.log(localMediaStream);
     video.src = window.URL.createObjectURL(localMediaStream);
     video.play();
   }).catch(function (err) {
     return console.error('ERROR: ', err);
+  });
+}
+
+function paintToCanavas() {
+  var width = video.videoWidth;
+  var height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(function () {
+    ctx.drawImage(video, 0, 0, width, height);
+
+    var pixels = ctx.getImageData(0, 0, width, height);
+    ctx.putImageData(rgbColorSplit(pixels), 0, 0);
   });
 }
 
@@ -27,12 +62,10 @@ function takePhoto() {
   var link = document.createElement('a');
   link.href = data;
   link.setAttribute('download', 'photo');
+  link.textContent = 'Download Image';
   link.innerHTML = '<img src="' + data + '" alt="photo"/>';
   strip.insertBefore(link, strip.firstChild);
-  // strip 比較像是 霸凌課
 }
-
-function paintToCanavas() {}
 
 getVideo();
 video.addEventListener('canplay', paintToCanavas);
