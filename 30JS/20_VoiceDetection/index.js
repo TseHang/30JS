840bcacd5a99c1f1810642b198193c85400adfdb
TseHang <a54383813@gmail.com>
2018-01-23 22:01:21 +0800

    Finish day 20

--- 30JS/20_VoiceDetection/index.js
+++ 30JS/20_VoiceDetection/index.js
@@ -5,26 +5,48 @@ const recognition = new SpeechRecognition();
 // 讓語音識別回傳識別後的資訊（預設為false)
 recognition.interimResults = true;
 
-const p = document.querySelector('.text');
+// let text = document.createElement('p');
+// const wrapper = document.querySelector('.wrapper');
+let text = document.createElement('p');
+const holdText = document.querySelector('.pre-hold');
 const wrapper = document.querySelector('.wrapper');
 const btnStart = document.querySelector('.btn-start');
 const btnStop = document.querySelector('.btn-stop');
 let isClickStop = false;
 
-wrapper.appendChild(p);
-
-// 監聽識別回傳
+// Fired when the speech recognition service returns a result,
+// a word or phrase has been positively recognized and this has been communicated back to the app.
 recognition.addEventListener('result', (e) => {
-  console.log(e.results);
+  const transcript = Array.from(e.results)
+    .map(result => result[0].transcript)
+    .join('');
+
+  text.textContent = transcript;
+  wrapper.appendChild(text);
+
+  // 如果回傳內容已經結束（一段話的結尾）在建立一個新的p元素來放下一段文字
+  if (e.results[0].isFinal) {
+    text = document.createElement('p');
+    wrapper.appendChild(text);
+
+    if (transcript.includes('笨蛋')) {
+      alert('Bingo!');
+      isClickStop = true;
+    }
+  }
 });
 
 // 監聽如果語音識別結束，則在開啟一次新的識別
-recognition.addEventListener('end', () => !isClickStop && recognition.start);
+recognition.addEventListener('end', () => !isClickStop && recognition.start());
 
 // 開始識別
-btnStart.addEventListener('click', () => recognition.start());
+btnStart.addEventListener('click', () => {
+  recognition.start();
+  holdText.textContent = 'Listening...';
+});
 btnStop.addEventListener('click', () => {
   isClickStop = true;
   recognition.stop();
+  holdText.textContent = 'I can hear you! ^_^';
 });
 
--- dist/js/20_VoiceDetection/index.js
+++ dist/js/20_VoiceDetection/index.js
@@ -7,29 +7,49 @@ var recognition = new SpeechRecognition();
 // 讓語音識別回傳識別後的資訊（預設為false)
 recognition.interimResults = true;
 
-var p = document.querySelector('.text');
+// let text = document.createElement('p');
+// const wrapper = document.querySelector('.wrapper');
+var text = document.createElement('p');
+var holdText = document.querySelector('.pre-hold');
 var wrapper = document.querySelector('.wrapper');
 var btnStart = document.querySelector('.btn-start');
 var btnStop = document.querySelector('.btn-stop');
 var isClickStop = false;
 
-wrapper.appendChild(p);
-
-// 監聽識別回傳
+// Fired when the speech recognition service returns a result,
+// a word or phrase has been positively recognized and this has been communicated back to the app.
 recognition.addEventListener('result', function (e) {
-  console.log(e.results);
+  var transcript = Array.from(e.results).map(function (result) {
+    return result[0].transcript;
+  }).join('');
+
+  text.textContent = transcript;
+  wrapper.appendChild(text);
+
+  // 如果回傳內容已經結束（一段話的結尾）在建立一個新的p元素來放下一段文字
+  if (e.results[0].isFinal) {
+    text = document.createElement('p');
+    wrapper.appendChild(text);
+
+    if (transcript.includes('笨蛋')) {
+      alert('Bingo!');
+      isClickStop = true;
+    }
+  }
 });
 
 // 監聽如果語音識別結束，則在開啟一次新的識別
 recognition.addEventListener('end', function () {
-  return !isClickStop && recognition.start;
+  return !isClickStop && recognition.start();
 });
 
 // 開始識別
 btnStart.addEventListener('click', function () {
-  return recognition.start();
+  recognition.start();
+  holdText.textContent = 'Listening...';
 });
 btnStop.addEventListener('click', function () {
   isClickStop = true;
   recognition.stop();
+  holdText.textContent = 'I can hear you! ^_^';
 });
