beceb498ec6c5d3bbe844d82ef356e70c4bd4e7e
Henry Hang <henry.h@17.media>
2018-01-30 20:17:07 +0800

    init day21

---
+++ dist/js/21_GeoSpeed/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// 取得HTML中的元素
+var arrow = document.querySelector('.arrow');
+var speed = document.querySelector('.speed-value');
+// 使用watchPosition來取得使用者的地理位置及海拔、速度
+navigator.geolocation.watchPosition(function (data) {
+  // 若成功取回，則會回傳一組Position(這裡定義名稱為data)
+  console.log(data);
+  // 使用coords.speed取回速度(公尺/秒)
+  speed.textContent = data.coords.speed;
+  // 使用coords.heading取得方位，代表偏離北方的角度，0為正北、90為正東
+  arrow.style.transform = 'rotate(' + data.coords.heading + 'deg)';
+}, function (err) {
+  // 錯誤回傳訊息，例如未取得定位授權時
+  console.error(err);
+});
